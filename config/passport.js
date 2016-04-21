658b53e53b027b31b52c2b19fba72ac19bfe21e1
wilsonfrench <wfrench12@gmail.com>
2016-04-21 16:02:19 -0400

    debugging

--- config/passport.js
+++ config/passport.js
@@ -73,7 +73,8 @@ module.exports = function(passport) {
 	passport.use(new FacebookStrategy({
 	    clientID: configAuth.facebookAuth.clientID,
 	    clientSecret: configAuth.facebookAuth.clientSecret,
-	    callbackURL: configAuth.facebookAuth.callbackURL
+	    callbackURL: configAuth.facebookAuth.callbackURL,
+	    profileFields: ['id', 'displayName','link', 'emails']
 	  },
 	  function(accessToken, refreshToken, profile, done) {
 	    	process.nextTick(function(){
@@ -87,7 +88,7 @@ module.exports = function(passport) {
 	    				newUser.facebook.id = profile.id;
 	    				newUser.facebook.token = accessToken;
 	    				newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-	    				//newUser.facebook.email = profile.emails[0].value;
+	    				newUser.facebook.email = profile.emails[0].value;
 
 	    				newUser.save(function(err){
 	    					if(err)
