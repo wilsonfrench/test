5276dc45d4d00dd688873d0eb9628b4138fc49f6
wilsonfrench <wfrench12@gmail.com>
2016-04-21 15:46:11 -0400

    debugging

--- config/passport.js
+++ config/passport.js
@@ -87,7 +87,7 @@ module.exports = function(passport) {
 	    				newUser.facebook.id = profile.id;
 	    				newUser.facebook.token = accessToken;
 	    				newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-	    				newUser.facebook.email = profile.emails[0].value;
+	    				//newUser.facebook.email = profile.emails[0].value;
 
 	    				newUser.save(function(err){
 	    					if(err)
