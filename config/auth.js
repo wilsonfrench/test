3ed67aa236ad36c0ea4104751dcd95a43d9bdb16
wilsonfrench <wfrench12@gmail.com>
2016-04-21 14:50:52 -0400

    first commit

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'facebookAuth':{
+		'clientID': "1598369417149379",
+		'clientSecret': "d6c390d038c807d00ff850709dde1cb5",
+		'callbackURL': "http://im-down.herokuapp.com/auth/facebook/callback" //or local host if changed on settings
+	}
+}
+
